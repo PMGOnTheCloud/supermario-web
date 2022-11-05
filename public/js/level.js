6cbefd69244079ab64dbff79a4d0742824f2cfc5
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-11-05 21:44:21 +0100

    Structuring tile data for future work on collision

--- public/js/level.js
+++ public/js/level.js
@@ -1,8 +1,17 @@
 import Compositor from './compositor.js';
+import { Matrix } from './math.js';
+
 
 export default class Level {
     constructor() {
-        this.comp = new Composition();
+        this.comp = new Compositor();
         this.entities = new Set();
+        this.tiles = new Matrix();
+    }
+
+    update(deltaTime) {
+        this.entities.forEach(entity => {
+            entity.update(deltaTime);
+        });
     }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
-import Compositor from './compositor.js';
 import Timer from './timer.js';
 import KeyboardState from './keyboard.js';
 import { loadLevel } from './loaders.js';
-import { loadBackgroundSprites } from './sprites.js';
 import { createMario } from './entities.js';
 
 
@@ -16,17 +14,17 @@ const context = canvas.getContext('2d');
 
 Promise.all([
     createMario(),
-    loadBackgroundSprites(),
     loadLevel('1-1')
 ])
 .then(([
     mario,
-    backgroundSprites,
     level
 ]) => {
         
     const gravity = 2000;
-    mario.pos.set(64, 180);
+    mario.pos.set(64, 64);
+
+    level.entities.add(mario);
 
     const SPACE = 32;
     const input = new KeyboardState();
@@ -45,8 +43,8 @@ Promise.all([
 
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
-        mario.update(deltaTime);
-        comp.draw(context);
+        level.update(deltaTime);
+        level.comp.draw(context);
         mario.vel.y += gravity * deltaTime;
     }
 
