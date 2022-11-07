2beca3279ea2ee5efee56786d3a930f2bdddadc3
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-11-07 18:04:26 +0100

    Drag and drop debug option to check tile

--- public/js/main.js
+++ public/js/main.js
@@ -38,6 +38,14 @@ Promise.all([
     input.listenTo(window);
 
 
+    ['mousedown', 'mousemove'].forEach(eventName => {
+        canvas.addEventListener(eventName, event => {
+            if (event.buttons === 1) {
+                mario.vel.set(0,0);
+                mario.pos.set(event.offsetX, event.offsetY);
+            }
+        });
+    });
     
 
 
