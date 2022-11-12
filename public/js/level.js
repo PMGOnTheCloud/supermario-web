8d42460d0fdca49a84a943f37c514fdbd7a725c6
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-11-13 00:59:57 +0100

    Chance block tiles animated

--- public/js/level.js
+++ public/js/level.js
@@ -5,6 +5,7 @@ import TileCollider from './tilecolider.js';
 export default class Level {
     constructor() {
         this.gravity = 2000;
+        this.totalTime = 0;
         this.comp = new Compositor();
         this.entities = new Set();
         this.tiles = new Matrix();
@@ -23,5 +24,7 @@ export default class Level {
 
             entity.vel.y += this.gravity * deltaTime;
         });
+        
+        this.totalTime += deltaTime;
     }
 }
--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,6 +1,8 @@
 import Level from './level.js';
 import SpriteSheet from './spritesheet.js';
 import { createBackgroundLayer, createSpriteLayer } from './layers.js';
+import { createAnim } from './anim.js';
+
 
 export function loadImage(url) {
     return new Promise(resolve => {
@@ -67,9 +69,16 @@ export function loadSpriteSheet(name) {
             });
         }
 
-        if(sheetSpec.frames) {
+        if (sheetSpec.frames) {
             sheetSpec.frames.forEach(frameSpec => {
                 sprites.define(frameSpec.name, ...frameSpec.rect);
+            });
+        }
+
+        if (sheetSpec.animations) {
+            sheetSpec.animations.forEach(animSpec => {
+                const animation = createAnim(animSpec.frames, animSpec.frameLen);
+                sprites.defineAnim(animSpec.name, animation);
             });
         }
 
