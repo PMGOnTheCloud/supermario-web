86465540ce4bf2f49c45a1bc2b52844e292d256f
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-11-14 20:27:09 +0100

    Added graceTime to jump and changed jump height

--- public/js/traits/jump.js
+++ public/js/traits/jump.js
@@ -5,9 +5,11 @@ export default class Jump extends Trait {
         super('jump');
     
         this.ready = 0;
-        this.duration = 0.5;
+        this.duration = 0.3;
         this.velocity = 200;
         this.engageTime = 0;
+        this.requestTime = 0;
+        this.gracePeriod = 0.15;
     }
 
     get falling() {
@@ -15,13 +17,12 @@ export default class Jump extends Trait {
     }
 
     start() {
-        if (this.ready > 0) {
-            this.engageTime = this.duration;
-        }
+        this.requestTime = this.gracePeriod;
     }
 
     cancel() {
         this.engageTime = 0;
+        this.requestTime = 0;
     }
 
     obstruct(entity, side) {
@@ -33,6 +34,15 @@ export default class Jump extends Trait {
     }
 
     update(entity, deltaTime) {
+
+        if (this.requestTime > 0) {
+            if (this.ready > 0) {
+                this.engageTime = this.duration;
+                this.requestTime = 0;
+            }
+
+            this.requestTime -= deltaTime;
+        }
         
         if (this.engageTime > 0) {
             entity.vel.y = -this.velocity;
