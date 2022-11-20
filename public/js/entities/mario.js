a3ec0a7f44df7be7b6ceea5db1cf8f1680ab98ab
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-11-20 15:03:29 +0100

    Refactor to make animations only be called once

--- public/js/entities/mario.js
+++ public/js/entities/mario.js
@@ -9,42 +9,44 @@ const FAST_DRAG = 1/5000;
 
 export function loadMario() {
     return loadSpriteSheet('mario')
-        .then(sprite => {
-            return function createMario() {
-                const mario = new Entity();
-                mario.size.set(14,16);
-                
-                mario.addTrait(new Go());
-                mario.go.dragFactor = SLOW_DRAG;
-                
-                mario.addTrait(new Jump());
+        .then(createMarioFactory);   
+}
 
-                mario.turbo = function setTurboState(turboOn) {
-                    this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
-                }
+function createMarioFactory(sprite) {
 
-                const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 8);
-                
-                function routeFrame(mario) {
-                    if (mario.jump.falling) {
-                        return 'jump';
-                    }
+    const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 8);
+        
+    function routeFrame(mario) {
+        if (mario.jump.falling) {
+            return 'jump';
+        }
 
-                    if (mario.go.distance > 0) {
-                        if ((mario.vel.x > 0 && mario.go.dir < 0) || (mario.vel.x < 0 && mario.go.dir > 0 )) {
-                            return 'break';
-                        }
-                        return runAnim(mario.go.distance);
-                    }
-                    return 'idle';
-                }
-
-
-                mario.draw = function drawMario(context) {
-                    sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
-                }
-            
-                return mario;
+        if (mario.go.distance > 0) {
+            if ((mario.vel.x > 0 && mario.go.dir < 0) || (mario.vel.x < 0 && mario.go.dir > 0 )) {
+                return 'break';
             }
-        });   
+            return runAnim(mario.go.distance);
+        }
+        return 'idle';
+    }
+
+    return function createMario() {
+        const mario = new Entity();
+        mario.size.set(14,16);
+        
+        mario.addTrait(new Go());
+        mario.go.dragFactor = SLOW_DRAG;
+        
+        mario.addTrait(new Jump());
+
+        mario.turbo = function setTurboState(turboOn) {
+            this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
+        }
+
+        mario.draw = function drawMario(context) {
+            sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
+        }
+
+        return mario;
+    }
 }
