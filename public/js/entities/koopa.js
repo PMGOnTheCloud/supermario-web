f82b0f2e2f6180b14060d742737e26657a2c2398
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-11-24 19:58:04 +0100

    Added as behaviour the pendulum walk

--- public/js/entities/koopa.js
+++ public/js/entities/koopa.js
@@ -1,4 +1,5 @@
 import Entity, { Sides } from "../entity.js";
+import PendulumWalk from "../traits/pendulumwalk.js";
 import { loadSpriteSheet } from '../loaders.js';
 
 
@@ -19,18 +20,7 @@ function createKoopaFactory(sprite) {
         const koopa = new Entity();
         koopa.size.set(16, 16);
 
-        koopa.addTrait({
-            NAME: 'walk',
-            speed: -30,
-            obstruct(koopa, side) {
-                if (side === Sides.LEFT || side === Sides.RIGHT) {
-                    this.speed = -this.speed;
-                }
-            },
-            update(koopa) {
-                koopa.vel.x = this.speed;
-            }
-        })
+        koopa.addTrait(new PendulumWalk());
 
         koopa.draw = drawKoopa;
         
