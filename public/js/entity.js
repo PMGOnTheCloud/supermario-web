afcc58577a2f115edd7b0038b9234d8c17467a62
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-11-25 19:50:47 +0100

    Refactor operations to class BoundingBox

--- public/js/entity.js
+++ public/js/entity.js
@@ -1,4 +1,5 @@
 import { Vec2 } from './math.js';
+import BoundingBox from './boundingbox.js';
 
 export const Sides = {
     TOP: Symbol('top'),
@@ -27,6 +28,8 @@ export default class Entity {
         this.pos = new Vec2(0, 0);
         this.vel = new Vec2(0, 0);
         this.size = new Vec2(0, 0);
+        this.offset = new Vec2(0, 0);
+        this.bounds = new BoundingBox(this.pos, this.size, this.offset);
         this.lifetime = 0;
     
         this.traits = [];
