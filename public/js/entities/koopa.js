52e52d65538c64593d40f9406d9a1f58338d3b78
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-12-07 22:11:13 +0100

    Double jumping on koopa makes shell move

--- public/js/entities/koopa.js
+++ public/js/entities/koopa.js
@@ -38,6 +38,9 @@ class Behaviour extends Trait {
     handleStomp(us, them) {
         if (this.state === STATE_WALKING) {
             this.hide(us);
+        } else if  (this.state === STATE_HIDING) {
+            us.killable.kill();
+            us.vel.set(100, 200);
         }
     }
 
