e1d0e65ae5a5ae13f12ce5cc44a4250dca8da4d0
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-12-11 21:31:30 +0100

    Font characters defined and loaded :)

--- public/js/loaders/font.js
+++ public/js/loaders/font.js
@@ -1,12 +1,21 @@
 import { loadImage } from '../loaders.js';
 import SpriteSheet from '../spritesheet.js';
 
+const CHARS = " !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
 export function loadFont() {
     return loadImage('/img/font.png')
         .then(image => {
             const fontSprite = new SpriteSheet(image);
 
-            fontSprite.define('A', 8, 16, 8, 8);
+            const size = 8;
+            const rowLen = image.width;
+
+            for (let [index, char] of [...CHARS].entries()) {
+                const x = index * size % rowLen;
+                const y = Math.floor(index * size / rowLen) * size;
+                fontSprite.define(char, x, y, size, size);
+            }
 
             return fontSprite;
         });
