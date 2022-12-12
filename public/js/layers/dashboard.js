9830f983a9a277b0b48e8938a04cbf3ac54c384a
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-12-12 19:12:59 +0100

    Added print method to write full string

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -1,5 +1,5 @@
 export function createDashboardLayer(font) {
     return function drawDashboard(context) {
-        font.draw('A', context, 0, 0);
+        font.print('Mario', context, 16, 8);
     }
 }
--- public/js/loaders/font.js
+++ public/js/loaders/font.js
@@ -3,6 +3,21 @@ import SpriteSheet from '../spritesheet.js';
 
 const CHARS = " !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 
+
+class Font {
+    constructor(sprites, size) {
+        this.sprites = sprites;
+        this.size = size;
+    }
+
+    print(text, context, x, y) {
+        [...text].forEach((char, pos) => {
+            this.sprites.draw(char, context, x + pos * this.size, y);
+        });
+    }
+}
+
+
 export function loadFont() {
     return loadImage('/img/font.png')
         .then(image => {
@@ -17,6 +32,6 @@ export function loadFont() {
                 fontSprite.define(char, x, y, size, size);
             }
 
-            return fontSprite;
+            return new Font(fontSprite, size);
         });
 }
