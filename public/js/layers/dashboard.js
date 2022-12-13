e507ca8ece631609cd21d3726d15db7ed8762d84
PMGOnTheCloud <pablomartingranara@gmail.com>
2022-12-13 14:16:03 +0100

    Added new font and almost completed dashboard

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -5,6 +5,8 @@ export function createDashboardLayer(font, playerEnv) {
 
     const score = 24500;
 
+    const coins = 13;
+
     return function drawDashboard(context) {
 
         const time = playerEnv.playerController.time;
@@ -12,7 +14,10 @@ export function createDashboardLayer(font, playerEnv) {
         font.print('MARIO', context, 16, LINE1);
         font.print(score.toString().padStart(6, '0'), context, 16, LINE2);
 
+        font.print('@x' + coins.toString().padStart(2, '0'), context, 96, LINE2);
+
         font.print('WORLD', context, 152, LINE1);
+        font.print('1-1', context, 160, LINE2);;
 
 
         font.print('TIME', context, 208, LINE1);
