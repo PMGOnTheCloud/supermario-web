c5e0720f614cf5fb6f32590a105e02fdd9783b83
PMGOnTheCloud <pablomartingranara@gmail.com>
2023-01-07 02:11:10 +0100

    Created gravity trait according to level

---
+++ public/js/traits/gravity.js
@@ -0,0 +1,12 @@
+import { Trait } from "../entity.js";
+
+export default class Gravity extends Trait {
+    constructor() {
+        super('gravity');
+    
+    }
+
+    update(entity, {deltaTime}, level) {
+        entity.vel.y += level.gravity * deltaTime;
+    }
+}
