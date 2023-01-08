49f543117a90a89ceae9328bbb9e48b5fba7372a
PMGOnTheCloud <pablomartingranara@gmail.com>
2023-01-08 02:24:41 +0100

    Created function that will emit bullet (hope so)

--- public/js/entities/cannon.js
+++ public/js/entities/cannon.js
@@ -12,12 +12,19 @@ export function loadCannon(audioContext) {
 
 function createCannonFactory(audio) {
 
+    function emitBullet(entity, level) {
+        console.log('Bullet fired', entity, level);
+    }
+
     return function createCannon() {
         const cannon = new Entity();
         
         cannon.audio = audio;
-        
-        cannon.addTrait(new Emitter());
+
+        const emitter = new Emitter();
+        emitter.emitters.push(emitBullet);
+
+        cannon.addTrait(emitter);
 
         return cannon;
     }
