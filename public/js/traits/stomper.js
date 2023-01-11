b3548afbf2b3e94b391f71082a6c237a0ac70444
PMGOnTheCloud <pablomartingranara@gmail.com>
2023-01-11 14:14:37 +0100

    Solved stomping on emitted bullets

--- public/js/traits/stomper.js
+++ public/js/traits/stomper.js
@@ -19,7 +19,7 @@ export default class Stomper extends Trait {
         }
 
         if (us.vel.y > them.vel.y) {
-            this.bounce(us, them);
+            this.queue(() => this.bounce(us, them));
             this.sounds.add('stomp');
             this.events.emit('stomp', us, them);
         }
